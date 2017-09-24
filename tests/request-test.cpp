a5450f758dc1f4d20f2a15836a7653d12653b019
limenghua <menghua.li@netbraintech.com>
2017-09-24 17:09:58 +0800

    more test

--- tests/request-test.cpp
+++ tests/request-test.cpp
@@ -12,3 +12,80 @@ TEST(Request, construct)
 {
 	RequestPtr req = Request::Create();
 }
+
+TEST(Request, Construct)
+{
+	RequestPtr res = Request::Create();
+
+	//CHECK_TRUE(false);
+}
+
+TEST(Request, PutSomeValues)
+{
+	RequestPtr res = Request::Create();
+
+	auto ptr = std::make_shared<int>(15);
+
+	res->Put("age", ptr);
+
+	auto val = res->Get<int>("age");
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val == ptr);
+	CHECK_EQUAL(*val, 15);
+}
+
+TEST(Request, PutSomeUserDefinedType)
+{
+	RequestPtr res = Request::Create();
+
+	struct MyStruct
+	{
+		int a;
+		char b;
+	};
+
+	auto ptr = std::make_shared<MyStruct>();
+	ptr->a = 3;
+	ptr->b = 'b';
+
+	res->Put("MyStruct", ptr);
+
+	auto val = res->Get<MyStruct>("MyStruct");
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val == ptr);
+	CHECK_EQUAL(val->a, 3);
+}
+
+TEST(Request, PutSomeUserDefinedTypeNoKeyName)
+{
+	RequestPtr res = Request::Create();
+
+	struct MyStruct
+	{
+		int a;
+		char b;
+	};
+
+	auto ptr = std::make_shared<MyStruct>();
+	ptr->a = 3;
+	ptr->b = 'b';
+
+	res->Put(ptr);
+
+	auto val = res->Get<MyStruct>();
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val == ptr);
+	CHECK_EQUAL(val->a, 3);
+}
+
+TEST(Request, GetNotExistValueShoudReturnNull)
+{
+	RequestPtr res = Request::Create();
+
+	auto val = res->Get<int>("nothiskey");
+
+	CHECK_TRUE(val == nullptr);
+}
--- tests/response-test.cpp
+++ tests/response-test.cpp
@@ -16,3 +16,73 @@ TEST(Response, Construct)
 
 	//CHECK_TRUE(false);
 }
+
+TEST(Response, PutSomeValues)
+{
+	ResponsePtr res = Response::Create();
+
+	auto ptr = std::make_shared<int>(15);
+
+	res->Put("age", ptr);
+
+	auto val = res->Get<int>("age");
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val==ptr);
+	CHECK_EQUAL(*val, 15);
+}
+
+TEST(Response, PutSomeUserDefinedType)
+{
+	ResponsePtr res = Response::Create();
+
+	struct MyStruct
+	{
+		int a;
+		char b;
+	};
+
+	auto ptr = std::make_shared<MyStruct>();
+	ptr->a = 3;
+	ptr->b = 'b';
+
+	res->Put("MyStruct", ptr);
+
+	auto val = res->Get<MyStruct>("MyStruct");
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val == ptr);
+	CHECK_EQUAL(val->a, 3);
+}
+
+TEST(Response, PutSomeUserDefinedTypeNoKeyName)
+{
+	ResponsePtr res = Response::Create();
+
+	struct MyStruct
+	{
+		int a;
+		char b;
+	};
+
+	auto ptr = std::make_shared<MyStruct>();
+	ptr->a = 3;
+	ptr->b = 'b';
+
+	res->Put(ptr);
+
+	auto val = res->Get<MyStruct>();
+
+	CHECK_TRUE(val != nullptr);
+	CHECK_TRUE(val == ptr);
+	CHECK_EQUAL(val->a, 3);
+}
+
+TEST(Response, GetNotExistValueShoudReturnNull)
+{
+	ResponsePtr res = Response::Create();
+
+	auto val = res->Get<int>("nothiskey");
+
+	CHECK_TRUE(val == nullptr);
+}
